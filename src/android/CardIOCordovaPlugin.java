7d509135f7a6e382fca9eae47de057b17a2b39c5
Hines Vaughan III <hvaughan3@gmail.com>
2018-06-07 11:43:54 -0500

    Adding missing import

--- src/android/CardIOCordovaPlugin.java
+++ src/android/CardIOCordovaPlugin.java
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 
 import io.card.payment.CardIOActivity;
 import io.card.payment.CreditCard;
